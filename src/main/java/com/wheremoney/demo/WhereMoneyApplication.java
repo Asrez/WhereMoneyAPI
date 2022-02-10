e8c0a1726c2ed8987b014341938ae9d40067fcff
amir shiati <amirshiati98@gmail.com>
2022-02-10 17:02:10 +0330

    Initial commit

---
+++ src/main/java/com/wheremoney/demo/WhereMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.wheremoney.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wheremoney/demo/WhereMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheremoney.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
